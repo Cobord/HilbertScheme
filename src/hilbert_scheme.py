4c88f5943b3c1501f553fac93543adbf22e4f956
Ammar Husain <ammar@horizonquantum.com>
2022-11-04 17:52:23 -0400

    permute axes, some plane partition

--- src/hilbert_scheme.py
+++ src/hilbert_scheme.py
@@ -1,4 +1,5 @@
-from typing import List, Optional, Set
+from __future__ import annotations
+from typing import Callable, Dict, List, Optional, Set, Tuple
 from abc import ABC, abstractmethod
 
 def AffineDSpacePt(d:int,field : type = complex):
@@ -30,6 +31,12 @@ def AffineDSpacePt(d:int,field : type = complex):
                 return AffineDSpacePt([self.my_field(zi*other.my_pt[i]) for i,zi in enumerate(self.my_pt)])
             else:
                 raise ValueError(f"Cannot coordinatewise multiply points in different fields or dimensions")
+        def permute(self,permutation : List[int]):
+            if sorted(permutation)==list(range(0,self.my_dimension)):
+                self.my_pt = [self.my_pt[idx] for idx in permutation]
+            else:
+                raise ValueError(f"{permutation} was not a permutation of the {self.my_dimension} coordinate axes")
+
     AffineDSpacePt.__name__ = 'A^%d (%s)' % (d,field.__name__)
     return AffineDSpacePt
 
@@ -37,11 +44,17 @@ class DPartition(ABC):
     @abstractmethod
     def n_value(self):
         pass
+    @abstractmethod
+    def d_value(self):
+        pass
+    @abstractmethod
+    def permute(self, permutation : List[int]):
+        pass
 
 class TwoDPartition(DPartition):
 
     @classmethod
-    def next_partition(cls,n : int, m : Optional[int] = None) -> 'TwoDPartition':
+    def next_partition(cls,n : int, m : Optional[int] = None) -> TwoDPartition:
         """Partition n with a maximum part size of m."""
         if n<0:
             raise ValueError(f"n must be non-negative")
@@ -56,14 +69,92 @@ class TwoDPartition(DPartition):
         self.my_partition = my_partition
         self.my_partition.sort(reverse=True)
         self.my_n = sum(self.my_partition)
-    def append(self,new_last : int) -> 'TwoDPartition':
+    def append(self,new_last : int) -> TwoDPartition:
         return TwoDPartition(self.my_partition + [new_last])
     def n_value(self) -> int:
         return self.my_n
+    def d_value(self) -> int:
+        return 2
+    def permute(self,permutation : List[int]):
+        if permutation == [0,1]:
+            pass
+        elif permutation == [1,0]:
+            new_partition = [0]*self.my_partition[0]
+            for idx in range(0,self.my_partition[0]):
+                new_partition[idx] = sum([part > idx for part in self.my_partition])
+            self.my_partition = new_partition
+        else:
+            raise ValueError(f"Only transposing is the proper permutation for a 2D partition")
     def as_list(self) -> List[int]:
         return self.my_partition
     def __str__(self) -> str:
         return str(self.my_partition)
+
+class ThreeDPartition(DPartition):
+
+    @classmethod
+    def next_partition(cls,n : int, rst : Optional[Tuple[int,int,int]]) -> ThreeDPartition:
+        """Plane Partition of n that fits in box of size (r,s,t)"""
+        if n<0:
+            raise ValueError(f"n must be non-negative")
+        raise NotImplementedError("Generator for all Plane Partitions not implemented")
+
+    def __init__(self,my_partition : Dict[Tuple[int,int],int]):
+        all_keys = my_partition.keys()
+        row_indices = [tup[0] for tup in all_keys]
+        col_indices = [tup[1] for tup in all_keys]
+        pi_ij_values = my_partition.values()
+        all_natural : Callable[[List[int]],bool] = lambda xs : all([x>=0 for x in xs])
+        if not(all_natural(row_indices)) or not(all_natural(col_indices)) or not(all_natural(pi_ij_values)):
+            raise ValueError("The i,j for pi_{i,j} must be natural numbers as well as the pi_{i,j} themselves")
+        if len(row_indices)==0:
+            self.my_partition = [[0]]
+            self.my_n = 0
+        else:
+            max_row = max(row_indices)
+            max_col = max(col_indices)
+            self.my_partition = [[0]*(max_col+1) for _ in range(max_row+1)]
+            for row_idx in range(0,max_row+1):
+                for col_idx in range(0,max_col+1):
+                    pi_ij = my_partition.get((row_idx,col_idx),0)
+                    self.my_partition[row_idx][col_idx] = pi_ij
+                    pi_ijp1 = my_partition.get((row_idx,col_idx+1),0)
+                    pi_ip1j = my_partition.get((row_idx+1,col_idx),0)
+                    if pi_ij<pi_ip1j or pi_ij<pi_ijp1:
+                        raise ValueError("pi_{i,j} fails to satisfy the required inequalities that make it interpretable as a plane partition")
+            self.my_n = sum(pi_ij_values)
+    def n_value(self) -> int:
+        return self.my_n
+    def d_value(self) -> int:
+        return 3
+    def __str__(self) -> str:
+        return str(self.my_partition)
+    def _cyclic_rotate(self) -> None:
+        raise NotImplementedError("No cyclic rotation [1,2,0] of plane partition")
+    def _xy_swap(self) -> None:
+        num_rows = len(self.my_partition)
+        num_cols = len(self.my_partition[0])
+        new_partition = [[self.my_partition[j][i] for j in range(0,num_rows)] for i in range(0,num_cols)]
+        self.my_partition = new_partition
+    def permute(self,permutation : List[int]):
+        if permutation == [0,1,2]:
+            pass
+        elif permutation == [1,0,2]:
+            self._xy_swap()
+        elif permutation == [1,2,0]:
+            self._cyclic_rotate()
+        elif permutation == [2,0,1]:
+            self._cyclic_rotate()
+            self._cyclic_rotate()
+        elif permutation == [0,2,1]:
+            self._cyclic_rotate()
+            self._cyclic_rotate()
+            self._xy_swap()
+        elif permutation == [2,1,0]:
+            self._cyclic_rotate()
+            self._xy_swap()
+        else:
+            raise ValueError(f"Only permutations of 0,1,2 are the proper permutation for a 3D partition")
 
 def HilbertScheme(n:int,d:int,field:type=complex):
     """
@@ -79,7 +170,7 @@ def HilbertScheme(n:int,d:int,field:type=complex):
         my_field : type = field
         
         @classmethod
-        def factory(cls,**kwargs) -> 'HilbertScheme':
+        def factory(cls,**kwargs) -> HilbertScheme:
             if not ( (zset := kwargs.get('zset',None)) is None):
                 from_set = HilbertSchemeSet(zset)
                 if from_set.fully_torus_fixed() and d==2:
@@ -91,11 +182,14 @@ def HilbertScheme(n:int,d:int,field:type=complex):
             else:
                 raise ValueError(f"Expected either set of {n} points in F^{d} or {d} dimensional partition of {n}")
         
-        def fully_torus_fixed(self):
+        def fully_torus_fixed(self) -> bool:
             return all(self.torus_fixed)
 
         @abstractmethod
         def rotate(self,multiplicative_factors:List[my_field]) -> None:
+            pass
+        @abstractmethod
+        def permute(self,my_permutation:List[int]) -> None:
             pass
 
     class HilbertSchemeTorusFixed(HilbertScheme):
@@ -107,14 +201,23 @@ def HilbertScheme(n:int,d:int,field:type=complex):
                 raise ValueError(f"Must be a partition of {n}")
             if isinstance(partition,TwoDPartition) and d==2:
                 self.partition = partition
+            elif isinstance(partition,ThreeDPartition) and d==3:
+                self.partition = partition
+            elif partition.d_value() == d:
+                raise NotImplementedError(f"d-dimensional partitions for d>3 not supported")
             else:
-                raise NotImplementedError(f"Partition must be a TwoDPartition")
+                raise ValueError(f"Partition's dimension must match with d")
 
         def __str__(self) -> str:
             return f"The monomial ideal of length {n} for A^{d} ({self.my_field.__name__}) specified by the partition {str(self.partition)}"
         
         def rotate(self,_) -> None:
             return
+        def permute(self,permutation : List[int]) -> None:
+            if sorted(permutation)==list(range(0,self.my_d)):
+                self.partition.permute(permutation)
+            else:
+                raise ValueError(f"{permutation} was not a permutation of the {self.my_d} coordinate axes")
     
     class HilbertSchemeSet(HilbertScheme):
         """
@@ -137,6 +240,7 @@ def HilbertScheme(n:int,d:int,field:type=complex):
             if not(len(self.zset)==n):
                 raise ValueError(f"Set not of size {n}")
             for i in range(d):
+                # CAUTION the type of field must have __eq__ such that __eq__(x,0) makes sense
                 self.torus_fixed[i] = True if all({pt.my_pt[i]==0 for pt in self.zset}) else False
         
         def __str__(self) -> str:
@@ -145,10 +249,16 @@ def HilbertScheme(n:int,d:int,field:type=complex):
         def rotate(self,multiplicative_factors : List[field]) -> None:
             if len(multiplicative_factors) != self.my_d:
                 raise ValueError(f"Expected {self.my_d} multiplicative factors")
+            # CAUTION the type of field must have __eq__ such that __eq__(x,0) makes sense
             if any([f==0 for f in multiplicative_factors]):
                 raise ValueError(f"Multiplicative factors cannot be 0")
             rotation_factor = self.point_class(multiplicative_factors)
             self.zset = {zi*rotation_factor for zi in self.zset}
             return
+        def permute(self,permutation : List[int]) -> None:
+            if sorted(permutation)==list(range(0,self.my_d)):
+                self.zset = {zi.permute(permutation) for zi in self.zset}
+            else:
+                raise ValueError(f"{permutation} was not a permutation of the {self.my_d} coordinate axes")
             
     return HilbertScheme
